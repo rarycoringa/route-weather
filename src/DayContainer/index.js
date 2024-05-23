f8671c4b47cb9eb09bf9a877a89afe82ae596867
Gabrienzo <gvplima09@gmail.com>
2024-05-23 20:48:42 -0300

    Criar seção de menu lateral (#9)

---
+++ src/DayContainer/index.js
@@ -0,0 +1,20 @@
+/*
+ função criar <CityCard>
+*/
+
+const DayContainer = (date) => {
+    return (
+        <></>
+    )
+}
+
+export default DayContainer
+
+/*
+    <DayContainerInside>
+        <DayHeader date={date}/> não sei se exporto ou faço aqui
+        <CityCard weatherDataCard = {weatherData}/> (0)
+        {...}
+        <CityCard/> (n)
+    </DayContainerInside>
+*/
--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
+import ErrorPage from "./Routes/error-page"
+import Timeline from './Routes/timeline'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      /*{
+        path: "/",
+        element: <Login />
+      },
+      */
+      {
+        path: "/Timeline",
+        element: <Timeline />
+      }
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+/*
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider> 
+  </React.StrictMode>
+*/
